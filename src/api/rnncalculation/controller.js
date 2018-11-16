1327386c48e1451688d53cf56a46c547d148616a
Magdiel Campelo Alves de Sousa <magdiel.camp@gmail.com>
2018-11-16 07:04:38 -0300

    a better method to rnn (same as matlab)

--- src/api/rnncalculation/controller.js
+++ src/api/rnncalculation/controller.js
@@ -1,9 +1,20 @@
 import Reading from '../reading/model';
-import Rnn from '../../services/tensorflow';
-import neataptic from 'neataptic';
 
-export const create = ({ body }, res, next) => {
+export const create = ({ body }, res) => {
   try {
+    var spawn = require('child_process').spawn,
+      py = spawn('python', ['./src/services/python/compute_pyrenn.py']),
+      dataString = '';
+
+    py.stdout.on('data', function(data) {
+      dataString = data.toString();
+    });
+    py.stdout.on('end', function() {
+      console.log(dataString);
+      var split = dataString.split('/');
+      res.status(201).send({ result: JSON.parse(split[1]) });
+    });
+
     var query = { sensor: body.sensor };
     Reading.countDocuments(query)
       .then(count =>
@@ -13,57 +24,14 @@ export const create = ({ body }, res, next) => {
         }))
       )
       .then(resp => {
-        var network = new neataptic.architect.LSTM(1, 4, 4, 1);
-        var output = resp.rows.map(el => parseFloat(el.value));
+        var data = resp.rows.map(el => [
+          parseFloat(el.value),
+          parseFloat(el.date)
+        ]);
 
-        var max = Math.max(...output) + 100;
-        var min = Math.min(...output) - 20;
-
-        var first = parseFloat(resp.rows[0].date);
-        var last = parseFloat(resp.rows[resp.count - 1].date) + body.predict;
-
-        var trainingData = resp.rows.map(el => ({
-          input: [(parseFloat(el.date) - first) / (last - first)],
-          output: [(parseFloat(el.value) - min) / (max - min)]
-        }));
-
-        network.train(trainingData, {
-          iterations: 20000,
-          error: 0.0001,
-          clear: true,
-          rate: 0.01
-        });
-        // var output = resp.rows.map(el => parseFloat(el.value));
-        var input = resp.rows.map(el => parseFloat(el.date));
-        console.log(input);
-        console.log(output);
-        var steps = new Array(body.predict / 25)
-          .fill(1)
-          .map((el, index) => last + 25 + index * 25);
-        var result = [];
-        steps.forEach(el => {
-          result.push(network.activate([(el - first) / (last - first)]) * max);
-        });
-        res.status(201).json({ result });
-
-        // var rnn = new Rnn(
-        //   Math.min(...output),
-        //   Math.max(...output) + 50,
-        //   input[0],
-        //   input[input.length - 1] + body.predict
-        // );
-        // rnn.createAndCompileModel(10);
-        // //train rnn
-        // rnn
-        //   .train(input, output)
-        //   .then(r => rnn.predict(steps))
-        //   .then(result => {
-        //     res.status(201).json({ result });
-        //   })
-        //   .catch(err => {
-        //     console.log('rnn error ' + err);
-        //     res.status(401).send(err);
-        //   });
+        var stream = { predict: body.predict, data, step: 25 };
+        py.stdin.write(JSON.stringify(stream));
+        py.stdin.end();
       })
       .catch(err => {
         console.log(err);
